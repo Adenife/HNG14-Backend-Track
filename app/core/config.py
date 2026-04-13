705ea62a99ee49760aae4120287738909c8f7e0f
Oluwanifemi Aweda <awedaoluwanifemi@gmail.com>
2026-04-13 14:50:57 +0100

    Updated app name

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Housing Project"
+    PROJECT_NAME: str = "Gender Classification System"
     PROJECT_VERSION: str = "1.0.0"
 
     GENDER_BASE_URL: str = "https://api.genderize.io/"
