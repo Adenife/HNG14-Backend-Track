364f6bd7ae3e5ae2a7799e68c8a767ab8054fcb3
Oluwanifemi Aweda <awedaoluwanifemi@gmail.com>
2026-05-05 12:31:25 +0100

    fix(alembic): Fixed alembic migrations

---
+++ alembic/versions/4c011b536bfd_add_profile_indexes.py
@@ -0,0 +1,28 @@
+"""add_profile_indexes
+
+Revision ID: 4c011b536bfd
+Revises: 58abf52992b5
+Create Date: 2026-05-05 12:30:48.121699
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4c011b536bfd'
+down_revision: Union[str, Sequence[str], None] = '58abf52992b5'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
